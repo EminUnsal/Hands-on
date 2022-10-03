c5e64da26b23e8a4a41a0fe963d5795d315c5760
mehmet <eminunsal20@gmail.com>
2022-10-03 08:18:47 +0200

    Roman-numbers added.

---
+++ python/cc-001-convert-to-roman-numerals/roman1.py
@@ -0,0 +1,17 @@
+def int_to_rome(x):
+    numbers = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+    roman = ['I','IV','V','IX', 'X','XL', 'L','XC', 'C','CD', 'D','CM','M']
+    i = 12
+    roman_numeral = ''
+    while x != 0:
+        if numbers[i] <= x:
+            roman_numeral += roman[i]
+            x = x - numbers[i]
+        else: 
+            i -= 1 # i = i-1
+    return roman_numeral
+
+print(int_to_rome(49))
+print(int_to_rome(4))
+print(int_to_rome(1249))
+print(int_to_rome(67))
