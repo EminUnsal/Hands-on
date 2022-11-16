0dfb5b24c49f8e63679a6282dcd261327ea6d987
mehmet <eminunsal20@gmail.com>
2022-11-16 16:28:19 +0100

    UserData.sh updated

--- DevOps/Terraform/S1_terraform-01_Installation_Basic_Operations/UserData.sh
+++ DevOps/Terraform/S1_terraform-01_Installation_Basic_Operations/UserData.sh
@@ -1,4 +1,6 @@
 # Wenn man irgendwo Terraform-Tools installieren will,klicke auf https://developer.hashicorp.com/terraform/downloads 
+
+#! /bin/bash
 sudo yum update -y
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
